7e7cc02a777aedeac8af43137de15113b16af657
Mendez lucio, Oscar [RNDBE] <OMendezL@its.jnj.com>
2021-06-04 12:52:18 +0200

    split get_deepdock_data.sh to get_CASF_2016.sh

---
+++ data/get_CASF_2016.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Download CASF-2016 from http://www.pdbbind.org.cn/casf.php
+wget http://www.pdbbind.org.cn/download/CASF-2016.tar.gz -O CASF-2016.tar.gz
+tar -zxvf CASF-2016.tar.gz
+rm CASF-2016.tar.gz
