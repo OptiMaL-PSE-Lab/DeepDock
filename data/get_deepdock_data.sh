6186e4d68adefc18b9c0df04507a08336d70d867
Mendez lucio, Oscar [RNDBE] <OMendezL@its.jnj.com>
2021-06-04 12:37:49 +0200

    remove CASF-2016.tar.gz after decompress in get_deepdock_data.sh

--- data/get_deepdock_data.sh
+++ data/get_deepdock_data.sh
@@ -8,3 +8,4 @@ wget https://ndownloader.figshare.com/files/27800643 -O dataset_deepdock_pdbbind
 # Download CASF-2016 from http://www.pdbbind.org.cn/casf.php
 wget http://www.pdbbind.org.cn/download/CASF-2016.tar.gz -O CASF-2016.tar.gz
 tar -zxvf CASF-2016.tar.gz
+rm CASF-2016.tar.gz
