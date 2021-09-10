f03eb051a8f312fe77389c853441972c95f95a73
Mendez lucio, Oscar [RNDBE] <OMendezL@its.jnj.com>
2021-09-10 11:36:35 +0200

    modify links in to download data preprocessed by Schrodinger

--- data/get_deepdock_data.sh
+++ data/get_deepdock_data.sh
@@ -2,6 +2,5 @@
 
 # Download PDBbind and CASF data for DeepDock from https://doi.org/10.6084/m9.figshare.c.5407329
 
-wget https://ndownloader.figshare.com/files/27800817 -O dataset_CASF-2016_285.tar
-wget https://ndownloader.figshare.com/files/27800643 -O dataset_deepdock_pdbbind_v2019_16K.tar
-
+wget https://ndownloader.figshare.com/files/30722306 -O dataset_CASF2016_Schrodinger_285_28a6b.tar
+wget https://ndownloader.figshare.com/files/30722387 -O dataset_deepdock_pdbbind_v2019_Schrodinger_17K_28a6b.tar
